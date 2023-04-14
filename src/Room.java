9a457b1dc0f39402fc17edeba33caf7fb0ceac09
Enter-Name <Enter-Name@outlook.com>
2023-04-14 11:30:41 -0400

    started Room abstract class

---
+++ src/Room.java
@@ -0,0 +1,6 @@
+public abstract class Room {
+    
+    private static int[] address; //room location
+    private static int[][] doors; //hasmap of the doors in the room to the room it goes to
+    private static String description; //description of the room
+}
